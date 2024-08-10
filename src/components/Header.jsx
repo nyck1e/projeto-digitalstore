fa71148800fd41d7cff78466f7bdcdd9cc3bf954
nicous <nyveamaria26@gmail.com>
2024-08-09 22:07:23 -0300

    header completo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom";
-import styled from "styled-components";
 import Logo from "./Logo";
 
 import './Header.css'
@@ -12,38 +11,61 @@ const Header = () => {
     return (
         <>
         <header>
-            <div>
-                <Logo/>
+            
+            <div className="lands">
+
+                <div>
+                    <Logo/>
+                </div>
+            
+                <div className="search">
+                    <InputText 
+                        type="text" 
+                        className="p-inputtext-lg" 
+                        placeholder="Pesquisar produtos..."
+                    />
+                <InputIcon   InputIcon className="pi pi-search"> </InputIcon>
+                </div>
+
+            </div>
+
+        <div className="interact">
+            
+                <div className="cadastro">
+                    <h4><a href="*">Cadastre-se</a></h4>
+                </div>
+
+                <div className="entrar">
+                    <button><a href="">Entrar</a></button>
+                </div>
+            
+                <div className="carrinho">
+                <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M5.42233 17.8203C5.84433 17.8203 6.18733 18.1633 6.18733 18.5853C6.18733 19.0073 5.84433 19.3493 5.42233 19.3493C5.00033 19.3493 4.65833 19.0073 4.65833 18.5853C4.65833 18.1633 5.00033 17.8203 5.42233 17.8203Z" stroke="#C92071" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M16.6747 17.8203C17.0967 17.8203 17.4397 18.1633 17.4397 18.5853C17.4397 19.0073 17.0967 19.3493 16.6747 19.3493C16.2527 19.3493 15.9097 19.0073 15.9097 18.5853C15.9097 18.1633 16.2527 17.8203 16.6747 17.8203Z" stroke="#C92071" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M0.749878 1.25L2.82988 1.61L3.79288 13.083C3.87088 14.018 4.65188 14.736 5.58988 14.736H16.5019C17.3979 14.736 18.1579 14.078 18.2869 13.19L19.2359 6.632C19.3529 5.823 18.7259 5.099 17.9089 5.099H3.16388" stroke="#C92071" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M12.1254 8.79504H14.8984" stroke="#C92071" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
             </div>
             
-            <div className="search">
-                <InputText 
-                    type="text" 
-                    className="p-inputtext-lg" 
-                    placeholder="Pesquisar produtos..."
-                />
-                <InputIcon className="pi pi-search"> </InputIcon>
-                
-            </div>
+        </div>
 
-            <div className="cadastro">
-                <h4><a href="*">Cadastre-se</a></h4>
-            </div>
+        <div className="void"></div>
 
-            <div className="entrar">
-                <button><a href="">Entrar</a></button>
-            </div>
 
+        <div className="navbar">
             <nav>
-                <ul>
-                    <li><NavLink to={"/"}>Home</NavLink></li>
-                    <li><NavLink to={"/produtos"}>Produtos</NavLink></li>
-                    <li><NavLink to={"*"}>Categorias</NavLink></li>
-                    <li><NavLink to={"*"}>Meus Pedidos</NavLink></li>
-                </ul>
+                
+                <a><NavLink to={"/"}>Home</NavLink></a>
+                <a><NavLink to={"/produtos"}>Produtos</NavLink></a>
+                <a><NavLink to={"*"}>Categorias</NavLink></a>
+                <a><NavLink to={"*"}>Meus Pedidos</NavLink></a>
+                
             </nav>
+        </div>
 
         </header>
+
         </>
      );
 }
