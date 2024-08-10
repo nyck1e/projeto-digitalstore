f3f6de54f64306b0256edde8dfb5b8a9b590fd37
nicous <nyveamaria26@gmail.com>
2024-08-10 18:33:23 -0300

    mudanças

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,63 +1,15 @@
 import './Footer.css'
+import Facebook from './icons/Facebook';
+import Instagram from './icons/Instagram';
+import Twitter from './icons/Twitter';
+import LogoF from './LogoF';
 
 const Footer = () => {
     return ( 
         <>
+
         <footer>
-
-            <div>
-                <h1>Logo</h1>
-            </div>
-
-            <div>
-                <p>lorem</p>
-            </div>
-
-            <div>
-                <h2>icones redes sociais</h2>
-            </div>
             
-            <div className='containers'>
-
-                <div className='box'>
-                    <h2 className='title'>Titulo</h2>
-                    <div className='informations'>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                    </div>
-                </div>
-
-                <div className='box'>
-                    <h2 className='title'>Titulo</h2>
-                    <div className='informations'>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                    </div>
-                </div>
-
-                <div className='box'>
-                    <h2 className='title'>Titulo</h2>
-                    <div className='informations'>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                        <h3>info</h3>
-                    </div>
-                </div>
-
-            </div>
-            
-            <div>
-                <hr />
-                <p>© {new Date().getFullYear()} Digital Store</p>
-            </div>
         </footer>
         </>
 
