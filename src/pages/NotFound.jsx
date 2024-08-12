4b996ea5c9bf3fe9846499432f386d5e43219e58
nicous <nyveamaria26@gmail.com>
2024-08-12 19:56:35 -0300

    commit final
    finalizei o projeto front end

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import Erro from "../assets/errorimage.jpg";
+import './Error404.css'
 
 const NotFound = () => {
     return ( 
         <>
-            <h2>Página não encontrada</h2>
-            <Link to={"/"}>Voltar para página principal</Link>
+            <div className="section">
+
+                <div className="img-size">
+                    <img src={Erro} alt="imagem erro 404" />
+                </div>
+                <div className="warning">
+                    <h1>Página não encontrada</h1>
+                    <Link to={"/"}><button>Voltar para página principal</button></Link>
+                </div>
+                
+            </div>
+            
         </>
      );
 }
